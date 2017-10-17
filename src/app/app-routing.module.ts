52b9628acf9a6b56200daa12ef1b7f24687b2158
deskach <dzianis.kachanau@gmail.com>
2017-10-17 21:36:09 +1100

    moved to hash-based routing strategy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -17,7 +17,7 @@ const appRoutes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(appRoutes)],
+  imports: [RouterModule.forRoot(appRoutes, { useHash: true })],
   exports: [RouterModule],
   declarations: []
 })
